7d3bd42e7613dc2e6b098da17a96f2a22b844469
fear <void.pohl@gmail.com>
2026-04-03 13:36:11 -0300

    Refatorar funções de verificação de senha e botão: modularizar lógica e melhorar clareza

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include "Fechadura.h"
 
 //Definição dos pinos
-
 int pin1 = 3;
 int pin2 = 2;
 int botao = 4;
@@ -12,10 +11,7 @@ int buzzer = 12;
 //Instancia da fechadura
 Fechadura fechadura(pin1, pin2, buzzer);
 
-String senhaDigitada;
-
 //Configuração do teclado matricial
-
 const byte ROWS = 4;
 const byte COLS = 3;
 char keys[ROWS][COLS] = {
@@ -28,8 +24,29 @@ byte rowPins[ROWS] = {5, 6, 7, 8};
 byte colPins[COLS] = {9, 10, 11};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-// Funções para verificar o teclado e o botão
+// Variável para armazenar a senha digitada
+String senhaDigitada;
 
+// Funções auxiliares
+void adicionarDigitoASenha(char key){
+  senhaDigitada += key;
+  Serial.println(senhaDigitada);
+}
+void limparSenhaDigitada(){
+  senhaDigitada = "";
+  Serial.println("Senha limpa.");
+}
+void verificarSenha(){
+  if (fechadura.autenticar(senhaDigitada)) {
+    Serial.println("Senha correta! Destrancando...");
+    fechadura.mudarStatusDeAuth();
+    fechadura.destrancar();
+  }
+  else {
+    Serial.println("Senha incorreta! Tente novamente.");
+    fechadura.biparBuzzer();
+  }
+}
 void verificarTeclado(){
   // Verificar a senha para destrancar pelo lado de fora
   char key = keypad.getKey();
@@ -37,31 +54,20 @@ void verificarTeclado(){
     // Verificar se a tecla pressionada é '#' para validar a senha ou '*' para limpar a senha digitada
     switch (key){
       case '#':
-        if (fechadura.autenticar(senhaDigitada)) {
-          Serial.println("Senha correta! Destrancando...");
-          fechadura.mudarStatusDeAuth();
-          fechadura.destrancar();
-        }
-        else {
-          Serial.println("Senha incorreta! Tente novamente.");
-          fechadura.biparBuzzer();
-        }
-        senhaDigitada = "";
+        verificarSenha();
+        limparSenhaDigitada();
         break;
 
       case '*':
-        senhaDigitada = "";
-        Serial.println("Senha limpa. Digite novamente.");
+        limparSenhaDigitada();
         break;
 
       default:
-        senhaDigitada += key;
+        adicionarDigitoASenha(key);
         fechadura.biparBuzzer();
-        Serial.println(senhaDigitada);
         break;
     }
   }
-
   else {
     if (key == '#'){
       fechadura.destrancar();
@@ -69,25 +75,13 @@ void verificarTeclado(){
     }
   }
 }
-void verificarBotao(){
-  // Verificar o estado do botão e da fechadura para trancar ou destrancar pelo lado de dentro
-  if (digitalRead(botao) == HIGH  && fechadura.statusDaAutenticacao() == false){ 
-    delay(50);
-    Serial.println("Destrancar...");
-    fechadura.biparBuzzer();
-    fechadura.destrancar();
-    fechadura.mudarStatusDeAuth();
-    while (digitalRead(botao) == HIGH);
-    delay(50);
+bool verificarSeOBotaoEstaPressionado(){
+  int buttonState = digitalRead(botao);
+  if (buttonState == HIGH) {
+    return true;
   }
-  else if (digitalRead(botao) == HIGH && fechadura.statusDaAutenticacao() == true){
-    delay(50);
-    Serial.println("Trancar...");
-    fechadura.biparBuzzer();
-    fechadura.trancar();
-    fechadura.mudarStatusDeAuth();
-    while (digitalRead(botao) == HIGH);
-    delay(50);
+  else {
+    return false;
   }
 }
 
@@ -100,5 +94,15 @@ void setup() {
 // Função de loop principal do Arduino
 void loop() {
   verificarTeclado();
-  verificarBotao();
+  if (verificarSeOBotaoEstaPressionado()){
+    if (fechadura.statusDaAutenticacao() == true){
+      Serial.println("Botão pressionado! Trancando...");
+      fechadura.trancar();
+      fechadura.mudarStatusDeAuth();
+    }
+    else {
+      fechadura.destrancar();
+      fechadura.mudarStatusDeAuth();
+    }
+  }
 }
